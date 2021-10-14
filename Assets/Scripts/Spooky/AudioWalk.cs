be9441833165fb5270d03a47b0caa55a8f2fb304
Josie DeRosier <s209055@students.aie.edu>
2021-10-13 17:52:34 -0700

    Working with audio

---
+++ Assets/Scripts/Spooky/AudioWalk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioWalk : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip[] sounds = new AudioClip[2];
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
